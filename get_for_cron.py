b356c3e1ab6946df10bd571614e20fe8d57f46b3
Artorius <Artorius.81@yandex.ru>
2023-12-29 19:08:51 +1000

    new files added

--- get_for_cron.py
+++ get_for_cron.py
@@ -3,7 +3,7 @@ import requests
 
 def send_get_request():
     response = requests.get("https://sterling-marmot-naturally.ngrok-free.app/api/vk_parse")
-    print(f"Response received: {response.status_code}")
+    print(f"Код получен: {response.status_code}")
 
 
 send_get_request()
